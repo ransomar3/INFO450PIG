e584361c2dd748fbf877741e034c25b829ab333a
ransomar3 <ransomar3@vcu.edu>
2017-10-10 02:53:00 -0400

    Added basic input for translating words and 2 character arrays (incomplete for now)

--- INFO450PIG/INFO450PIG.cpp
+++ INFO450PIG/INFO450PIG.cpp
@@ -2,10 +2,20 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
+int enWord;
+char words[50];
+char translate[50];
 
 
 int main()
+
 {
+	cout << "This is an English to Pig Latin translator." << endl;
+	cout << "Please enter the word you would like to translate here: " << endl;
+	cin >> enWord;
+
     return 0;
 }
 
