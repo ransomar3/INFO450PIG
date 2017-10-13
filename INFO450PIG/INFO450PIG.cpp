803dd5b5d8ceb21e9887b541f7d6c0b240fc00a3
ransomar3 <ransomar3@vcu.edu>
2017-10-12 23:50:12 -0400

    Added switch function to detect vowels in inputs. Added translateToPigLatin function and a rotate function to replace the first letter in each word that's not a vowel and put the word at the end of it.

--- INFO450PIG/INFO450PIG.cpp
+++ INFO450PIG/INFO450PIG.cpp
@@ -1,35 +1,77 @@
-// INFO450PIG.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include <iostream>
+#include <stdio.h>
 #include <string>
 using namespace std;
 
-char enWord;
-char firstLetter;
-char words[50];
-char translate[50];
-
+bool isAVowel(char);
+string translateToPigLatin(string);
 
 int main()
-
 {
-	cout << "This is an English to Pig Latin translator." << endl;
-	cout << "Please enter the word you would like to translate here: " << endl;
-	gets_s(words);
-	cout << "You entered " << words << endl; 
-	
-	bool vowel;
+	string fromEnglish;
+	cout << "Please enter a word you would like to translate:\n " << endl;
+	cin >> fromEnglish;
+	cout << fromEnglish << "\nThat would be: " << translateToPigLatin(fromEnglish) << " in Pig Latin.\n" << endl;
 
-	if (firstLetter == 'a' || firstLetter == 'e' || firstLetter == 'i' || firstLetter == 'o' || firstLetter == 'u')
-	{
-		cout << "That would be " << enWord << "ay!" << endl; 
-	}
-	else
-	{
-
-	}
-    return 0;
+	return 0;
 }
 
+bool isAVowel(char ch)
+{
+	switch (ch)
+	{
+	case 'A':
+	case 'E':
+	case 'I':
+	case 'O':
+	case 'U':
+	case 'a':
+	case 'e':
+	case 'i':
+	case 'o':
+	case 'u':
+		return true;
+	default:
+		return false;
+	}
+}
+
+string rotate(string str)
+{
+	string::size_type len = str.length();
+	string toPigLatin;
+	toPigLatin = str.substr(1, len - 1) + str[0];
+	return toPigLatin;
+	// Brings the first letter of the word you're translating to the end
+}
+
+string translateToPigLatin(string word)
+{
+	// If the string is a vowel, it adds "ay" to the end of it. 
+	string::size_type len;
+	bool vowel;
+	string::size_type counter;
+	if (isAVowel(word[0]))
+		word = word + "ay";
+	else
+	{
+		word = word + "";
+		word = rotate(word);
+		len = word.length();
+		vowel = false; 
+		for (counter = 1; counter < len - 1; counter++)
+			if (isAVowel(word[0]))
+			{
+				vowel = true;
+				break;
+			}
+			else
+				word = rotate(word);
+		if (!vowel)
+			word = word.substr(1, len) + "ay";
+		else
+			word = word + "ay";
+	}
+	return word;
+}
